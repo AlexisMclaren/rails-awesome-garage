a531aaa7e2667b8ad74d4c92faf747bc1c61008d
Alexis Mclaren mclaren370@gmail.com <mclaren370@gmail.com>
2025-05-15 15:18:06 +0100

    Completed up to section 5.

---
+++ db/migrate/20250515133752_add_owner_to_cars.rb
@@ -0,0 +1,5 @@
+class AddOwnerToCars < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :cars, :owner, null: false, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning the DB..."
+
+Car.destroy_all
+Owner.destroy_all
+
+puts "Creating owners..."
+ife = Owner.create!(nickname: "ife5000")
+deji = Owner.create!(nickname: "DejiGasGas")
+claire = Owner.create!(nickname: "WonderClair")
+ben = Owner.create!(nickname: "BenMcClaren")
+
+puts "Creating cars..."
+
+Car.create!(brand: "Farrari", model: "Testarossa", year: 1995, fuel: "Unleaded Petrol", owner: ife)
+Car.create!(brand: "Mercedes", model: "300SL Roadster", year: 1987, fuel: "Diesel", owner: deji)
+Car.create!(brand: "Aston Martin", model: "DB5", year: 1965, fuel: "Unlead Petrol", owner: claire)
+Car.create!(brand: "Mclaren", model: "720S", year: 2024, fuel: "Unlead Petrol", owner: ben)
+
+puts "Done! #{Owner.count} owners and #{Car.count} cars created."
